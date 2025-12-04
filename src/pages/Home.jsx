bff8f52a06ea87a5ecc4b223668d8d1773275dc0
MalihaAkram14 <malihaakram86@gmail.com>
2025-12-04 16:30:12 +0500

    Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,16 @@
 import Hero from "../components/Hero";
 import Categories from "../components/Categories";
 import { Helmet } from "react-helmet";
+import Footer from "../components/Footer";
 
 
 const Home = () => {
   return (
     <div className="min-h-screen bg-red-200">
       <Hero />
-    
+     <Footer />
     </div>
+   
   );
 };
 
