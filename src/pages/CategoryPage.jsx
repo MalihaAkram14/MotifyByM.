47536a843506fd9723eb6ecb069b8c6dff73faaf
MalihaAkram14 <malihaakram86@gmail.com>
2025-12-04 16:29:47 +0500

    CategoryPage.jsx

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -1,53 +1,37 @@
-import { Link, useParams } from "react-router-dom";
-import products from "../data/products.json";
-import { Helmet } from "react-helmet";
-
+import { useParams, useSearchParams } from "react-router-dom";
+import productsData from "../data/products.json";
+import ProductGrid from "../components/ProductGrid";
+import Pagination from "../components/Pagination";
+import usePagination from "../hooks/usePagination";
 
 const CategoryPage = () => {
-  const { categoryName } = useParams();
+  const { categoryName ,page} = useParams(); // /:cate
+  console.log("page",page) 
+  useSearchParams()
 
-  const filteredProducts = products.filter(
+  // Filter products by category
+  const filteredProducts = productsData.filter(
     (p) => p.category.toLowerCase() === categoryName.toLowerCase()
   );
 
+  // Use pagination hook
+  const { currentPage, totalPages, currentItems, goToPage } = usePagination(filteredProducts, 4);
+
   return (
-    <div className="pt-20 px-6">
-      <h1 className="text-3xl font-bold mb-6 capitalize text-center">
-        {categoryName} Products
-      </h1>
+    <div className="pt-24 px-8 pb-16">
+      <h2 className="text-3xl font-bold text-center mb-8">{categoryName}</h2>
 
-      {filteredProducts.length === 0 ? (
-        <p>No products found in this category.</p>
-      ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mb-8">
-          {filteredProducts.map((product) => (
-            <div key={product.id} className="border p-4 rounded shadow">
+      {/* Product list */}
+      <ProductGrid products={currentItems} />
 
-              {/* CLICK to go to Product Details */}
-              <Link to={`/product/${product.id}`}>
-                <img
-                  src={product.image}
-                  alt={product.name}
-                  className="w-full h-40 object-cover rounded"
-                />
-                <h2 className="font-semibold mt-2">{product.name}</h2>
-                <p className="text-gray-600">${Math.abs(product.price)}</p> 
-                <p className="text-gray-600">${product.description}</p>
-              </Link>
+      <Pagination
+  currentPage={currentPage}
+  totalPages={totalPages}
+  goToPage={goToPage}
+/>
 
-               
-              <button className="mt-3 w-full bg-black text-white py-2 rounded hover:bg-gray-800 ">
-                Add to Cart
-              </button>
-
-            </div>
-          ))}
-        </div>
-      )}
     </div>
   );
 };
 
 export default CategoryPage;
-
-
